f67489275b5e04c2e1c70dc4d532b6d0617a81f0
John Schneider <johnschneider@Johns-MacBook-Pro.local>
2022-02-24 08:35:32 -0600

    DominatingPriorityQueue passes initial tests

---
+++ JeevesTest/DominatingPriorityQueueTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jeeves;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JeevesTest
+{
+	[TestClass]
+	public class DominatingPriorityQueueTest
+	{
+		[TestMethod]
+		public void DominatingPriorityQueueInitTest1()
+		{
+			DominatingPriorityQueue<int> PQ = new DominatingPriorityQueue<int>();
+			Assert.IsTrue(PQ.IsEmpty());
+		}
+		[TestMethod]
+		public void DominatingPriorityQueueInitTest2()
+        {
+			DominatingPriorityQueue<int> PQ = new DominatingPriorityQueue<int>(1);
+			Assert.IsFalse(PQ.IsEmpty());
+        }
+		[TestMethod]
+		public void DominatingPriorityQueuePullTest1()
+		{
+			int init = 1;
+			DominatingPriorityQueue<int> PQ = new DominatingPriorityQueue<int>(init);
+			int head = PQ.PullHead();
+			Assert.AreEqual(init, head);
+			Assert.IsTrue(PQ.IsEmpty());
+		}
+		[TestMethod]
+		public void DominatingPriorityQueuePullTest2()
+        {
+			int[] init = { 1, 2 };
+			DominatingPriorityQueue<int> PQ = new DominatingPriorityQueue<int>();
+			init.ToList().ForEach(x => PQ.Insert(x));
+			int head = PQ.PullHead();
+			Assert.AreEqual(1, head);
+			head = PQ.PullHead();
+			Assert.AreEqual(2, head);
+			Assert.IsTrue(PQ.IsEmpty());
+        }
+		[TestMethod]
+		public void DominatingPriorityQueuePullTest3()
+		{
+			int[] init = { 2, 1 };
+			DominatingPriorityQueue<int> PQ = new DominatingPriorityQueue<int>();
+			init.ToList().ForEach(x => PQ.Insert(x));
+			int head = PQ.PullHead();
+			Assert.AreEqual(1, head);
+			head = PQ.PullHead();
+			Assert.AreEqual(2, head);
+			Assert.IsTrue(PQ.IsEmpty());
+		}
+	}
+}
+
---
+++ JeevesTest/ScheduleTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jeeves;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JeevesTest
+{
+    [TestClass]
+    public class SchedulerTests
+    {
+        [TestMethod]
+        public void ExampleSchedule1()
+        {
+            //job = {identifier, value, releaseTime, processTime, dueDate, deadLine}
+            int[,] jobMatrix =
+            {
+                {1, 2, 0, 3, 7, 8},
+                {2, 3, 2, 2, 9, 10},
+                {3, 4, 3, 3, 8, 10},
+                {4, 2, 6, 2, 13, 14}
+            };
+            List<Job> jobs = Job.CreateJobsFromMatrix(jobMatrix);
+            int[,] setupMatrix =
+            {
+                {1, 1, 1, 1, 2},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 2, 1, 2, 1}
+            };
+            SetupTime setupTime = (Job i, Job j) => setupMatrix[i.Identity, j.Identity];
+            List<(Job, int)> scheduledJobs = Scheduler.Schedule(jobs, setupTime).ToList<(Job, int)>();
+
+            List<(Job, int)> solution = new List<(Job, int)>(new (Job, int)[]
+            {
+                (jobs[1], 2),
+                (jobs[2], 5),
+                (jobs[3], 9)
+            });
+            CollectionAssert.AreEqual(scheduledJobs, solution);
+        }
+    }
+}
