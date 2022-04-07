213fbd4a241ad6d6a33c055f5be7155758ebe9d7
John Schneider <johnschneider@Johns-MacBook-Pro.local>
2022-04-06 21:34:51 -0500

    Updated ToScheduleTime and FromScheduleTime to take an argument defining the current time

--- Jeeves/UserPreferences.cs
+++ Jeeves/UserPreferences.cs
@@ -15,29 +15,38 @@ namespace Jeeves
 		private TimeSpan schedulingFidelity { get; set; }
 		private TimeSpan workdayStart { get; set; }
 		private TimeSpan workdayLength { get; set; }
+		private int daysInInterval { get; set; }
+		private TimeSpan restTime
+		{
+			get
+			{
+				return (workdayStart - (workdayStart + workdayLength).mod(day())).mod(day());
+			}
+		}
 
 		public TimeZoneInfo GetTimeZone() =>
 			TimeZoneInfo.FindSystemTimeZoneById(timeZone);
 		// this all only works for intervals of less than 24 hours
 		private bool withinWorkInterval(TimeSpan timeLocal) =>
-			timeSinceWorkIntervalStart(timeLocal) <= workdayLength;
-		private TimeSpan timeSinceWorkIntervalStart(TimeSpan timeLocal) =>
-			(timeLocal - workdayStart).modTimeSpan(new TimeSpan(24, 0, 0));
-		private TimeSpan timeUntilWorkIntervalStart(TimeSpan timeLocal) =>
-			(workdayStart - timeLocal).modTimeSpan(new TimeSpan(24, 0, 0));
+			timeSinceWorkDayStart(timeLocal) <= workdayLength;
+		private TimeSpan timeSinceWorkDayStart(TimeSpan timeLocal) =>
+			(timeLocal - workdayStart).mod(new TimeSpan(24, 0, 0));
+		private TimeSpan timeUntilWorkdayStart(TimeSpan timeLocal) =>
+			(workdayStart - timeLocal).mod(new TimeSpan(24, 0, 0));
 		// If 'now' is in a workday, returns the date and time of the start of that workday
 		// If 'now' is outside of a workday, returns the date and time of the start of the next workday
 		// argument 'now' is in UTC
-		private DateTime workdayStartUTC(DateTime now) {
+		private DateTime workIntervalStartUTC(DateTime now) {
 			DateTime nowLocal = TimeZoneInfo.ConvertTimeFromUtc(now, GetTimeZone());
 			DateTime workdayStartLocal = withinWorkInterval(nowLocal.TimeOfDay) ?
-				nowLocal - timeSinceWorkIntervalStart(nowLocal.TimeOfDay) :
-				nowLocal + timeUntilWorkIntervalStart(nowLocal.TimeOfDay);
+				nowLocal - timeSinceWorkDayStart(nowLocal.TimeOfDay) :
+				nowLocal + timeUntilWorkdayStart(nowLocal.TimeOfDay);
 			return TimeZoneInfo.ConvertTimeToUtc(workdayStartLocal, GetTimeZone());
 		}
 
-		private DateTime workdayEndUTC(DateTime now) =>
-			workdayStartUTC(now) + workdayLength;
+
+
+
 
 		public UserPreferences()
 		{
@@ -47,20 +56,21 @@ namespace Jeeves
 			throw new NotImplementedException();
 
 
-		// todo all public facing methods of UserPreferences need to take in an argument 'DateTime now'
+		// all public facing methods of UserPreferences need to take in an argument 'DateTime now'
 		// in order to define what the realtime workday is. 
-		public int ToScheduleTime(DateTime time) =>
-			(int)Math.Round(
-				(timeSinceWorkdayStart(time).modTimeSpan(day()).clampTimeSpan(TimeSpan.Zero, workdayLength())
-					+ maxOneDayDuration(timeSinceWorkdayStart(time)) * workdayLength())
-				/ SchedulingFidelity);
-		public DateTime FromScheduleTime(int time) =>
-			DateTime.Today + scheduleTimeDaysOnly(time) + scheduleTimeTimeOnly(time) + workdayStartUTC().TimeOfDay;
+		public int ToScheduleTime(DateTime time, DateTime now) =>
+			ToScheduleDuration(time - workIntervalStartUTC(now));
+
+		public DateTime FromScheduleTime(int time, DateTime now) =>
+			workIntervalStartUTC(now)
+				+ ((time / scheduleWorkdayLength()) * (workdayLength + restTime))
+				+ (time % scheduleWorkdayLength()) * schedulingFidelity;
+
 		public int ToScheduleDuration(TimeSpan duration) =>
-			(int)Math.Round(
-				(maxOneDayDuration(duration) * workdayLength() + timeOnlyDuration(duration))
-				/ SchedulingFidelity
-				);
+			scheduleWorkdayLength() * Math.Min(daysInInterval, (int)Math.Floor(duration / (workdayLength + restTime)))
+				+ (int)Math.Round(TimeExtensions.min(workdayLength, duration.mod(workdayLength + restTime)) / schedulingFidelity);
+
+		//todo
 		public int ValueByCategory(string category, bool isDaily, DateTime created, DateTime? lastCompleted) =>
 			isDaily ?
 				10 + Math.Min((DateTime.Now - (lastCompleted.HasValue? (DateTime)lastCompleted : DateTime.MinValue)).Days, 10) :
@@ -70,24 +80,8 @@ namespace Jeeves
 					_ => 1 + (DateTime.Now - created).Days
 				};
 
-
 		private int scheduleWorkdayLength() =>
-			(int)Math.Round(workdayLength() / SchedulingFidelity);
-
-		private TimeSpan scheduleTimeDaysOnly(int time) =>
-			new TimeSpan(time / scheduleWorkdayLength(), 0, 0, 0);
-
-		private TimeSpan scheduleTimeTimeOnly(int time) =>
-			(time % scheduleWorkdayLength()) * SchedulingFidelity;
-
-		private int maxOneDayDuration(TimeSpan duration) =>
-			Math.Min(1, duration.Days);
-
-		private TimeSpan timeSinceWorkdayStart(DateTime time) =>
-			time - workdayStartUTC();
-
-		private TimeSpan timeOnlyDuration(TimeSpan duration) =>
-			new TimeSpan(duration.Hours, duration.Minutes, duration.Seconds);
+			(int)Math.Round(workdayLength / schedulingFidelity);
 
 		private TimeSpan day() =>
 			new TimeSpan(24, 0, 0);
@@ -95,9 +89,11 @@ namespace Jeeves
 	}
 	public static class TimeExtensions
     {
-		public static TimeSpan modTimeSpan(this TimeSpan a, TimeSpan b) =>
+		public static TimeSpan mod(this TimeSpan a, TimeSpan b) =>
 			new TimeSpan(a.Ticks % b.Ticks);
-		public static TimeSpan clampTimeSpan(this TimeSpan a, TimeSpan minValue, TimeSpan maxValue) =>
+		public static TimeSpan min(this TimeSpan a, TimeSpan b) =>
+			new TimeSpan(Math.Min(a.Ticks, b.Ticks));
+		public static TimeSpan clamp(this TimeSpan a, TimeSpan minValue, TimeSpan maxValue) =>
 			new TimeSpan(Math.Clamp(a.Ticks, minValue.Ticks, maxValue.Ticks));
 	}
 }
